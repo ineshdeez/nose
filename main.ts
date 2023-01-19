7a693c68f77047f9cccedeb940220da20a5dc6ae
ineshdeez <122534302+ineshdeez@users.noreply.github.com>
2023-01-19 13:50:24 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    numero += 1
+    basic.showNumber(numero)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (numero > 5) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    numero += -1
+    basic.showNumber(numero)
+})
+let numero = 0
+numero = 0
+basic.showNumber(numero)
 basic.forever(function () {
 	
 })
